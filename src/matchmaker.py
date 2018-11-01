47edbfa19f87695a1f66fa4964ed03428ae61fb2
Athight <j.lallu25@gmail.com>
2018-11-02 02:11:39 +1300

    quick starting template

--- src/matchmaker.py
+++ src/matchmaker.py
@@ -1,9 +1,18 @@
 #!/usr/bin/env python3
 
+import entities
+import currency
+import matching
+
 class Matchmaker:
 
-	def __init__(self, database):
+	def __init__(self, database, currency):
 		self._database = database
+		self._currency = currency
+
+		self._entities = None
+		self._charityCache = {}
+		self._countryCache = {}
 
 	def find_match(self, offer_id):
 		'''Returns the id of a matching offer, or None.'''
@@ -15,6 +24,36 @@ class Matchmaker:
 		# offer MUST have different email address
 		# offer SHOULD have approximately the same amount (taking tax benefits into account)
 		# (See https://www.ietf.org/rfc/rfc2119.txt for "MUST" and "SHOULD")
+
+		# TODO: Smart Caching
+		self._entities = entities.Entities(self._database)
+
+		charities = self._entities.charities()
+		countries = self._entities.countries()
+		offers = [] #self._entities.offers()
+
+		allOffers = []
+
+		print("CHARITIES")
+		for charityToBuild in charities:
+			print(charityToBuild)
+
+		print("COUNTRIES")
+		for countryToBuild in countries:
+			print(countryToBuild)
+
+		print("OFFERS")
+		for offerToBuild in offers:
+			print(offerToBuild)
+
+		with self._database.connect() as db:
+			thisOfferToBuild = db.read_one('SELECT * FROM offers WHERE id = %(id)s ', id=offer_id)
+			thisOffer = None
+
+		matching_offer = matching.matcher.Matcher("USD").match(thisOffer, allOffers)
+
+		# TODO: finish implementation
+		# fallback to marc's stuff
 
 		# for now, let's just return a random offer
 		with self._database.connect() as db:
