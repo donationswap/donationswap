6953ee27ef446ea0b6cfa236afb1e8bc3825a92d
Athight <j.lallu25@gmail.com>
2018-11-18 15:35:12 +1300

    Update 2018-11-18_gift_aid.sql

--- src/sql/upgrades/2018-11-18_gift_aid.sql
+++ src/sql/upgrades/2018-11-18_gift_aid.sql
@@ -1,13 +1,13 @@
 ALTER TABLE countries
-ADD COLUMN gift_aid FLOAT NOT NULL DEFAULT 0;
+ADD COLUMN gift_aid INT NOT NULL DEFAULT 0;
 
 ALTER TABLE countries
 ALTER COLUMN gift_aid DROP DEFAULT;
 
 UPDATE TABLE countries
-SET gift_aid=0.25
+SET gift_aid=25
 WHERE iso_name='GB';
 
 UPDATE TABLE countries
-SET gift_aid=0.45
+SET gift_aid=45
 WHERE iso_name='IE';
