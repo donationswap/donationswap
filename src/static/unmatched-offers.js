1ca6a7452de76dc80b27129b5bbaadc4b6a19348
bigfighter <marc.spoor@gmail.com>
2018-11-21 10:33:00 +0000

    unmatched offers now show amount for charity, not amount offered

--- src/static/unmatched-offers.js
+++ src/static/unmatched-offers.js
@@ -10,7 +10,7 @@
 	};
 
 	function getSummary(offer) {
-		return `${offer.country}, ${offer.charity}, ${offer.min_amount_localized}-${offer.amount_localized} ${offer.currency_localized}`;
+		return `${offer.country}, ${offer.charity}, ${offer.min_amount_for_charity_localized}-${offer.amount_for_charity_localized} ${offer.currency_localized}`;
 	}
 
 	function renderDetails(details) {
