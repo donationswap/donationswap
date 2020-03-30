9c0940f47e39938f0cff55861af21e3322dd2b1f
Maxwell Clarke <maxeonyx@gmail.com>
2020-03-30 19:08:49 +1300

    Test data.
    This data can be used for testing the website locally. After running the initial schema and upgrade scripts, run these data scripts to get a working database.
    
    I have removed the currencies migration script. This does not need to be run again, I consider it normal data rather than reference data.

---
+++ src/sql/test_data/04-charities_in_countries.sql
@@ -0,0 +1,12 @@
+insert into charities_in_countries
+    (charity_id, country_id, instructions)
+values
+    (0,          0,         'Donate through the AMF website. https://www.againstmalaria.com/Donation.aspx'),
+    (0,          1,         'Donate through the AMF website. https://www.againstmalaria.com/Donation.aspx'),
+    (0,          2,         'Donate through the AMF website. https://www.againstmalaria.com/Donation.aspx'),
+    (0,          3,         'Donate through the AMF website. https://www.againstmalaria.com/Donation.aspx'),
+    (1,          2,         'Donate through Effective Alruism Funds.'),
+    (3,          2,         'Donate through Effective Alruism Funds.'),
+    (2,          0,         'Donate through Oxford University.'),
+    (2,          1,         'Donate through Oxford University.');
+
