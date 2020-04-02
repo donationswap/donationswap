33568fc7fd20405d7810fc5e408c8ec88526107c
Maxwell Clarke <maxeonyx@gmail.com>
2020-04-02 14:45:35 +1300

    Script for easy creation of a test database.

---
+++ src/sql/create_test_database.sql
@@ -0,0 +1,16 @@
+\ir schema.sql
+
+\ir upgrades/2018-11-08_declined_matches.sql
+\ir upgrades/2018-11-09_name_min_amount.sql
+\ir upgrades/2018-11-10_eventlog.sql
+\ir upgrades/2018-11-12_admin.sql
+\ir upgrades/2018-11-15_eventlog.sql
+\ir upgrades/2018-11-16_admin.sql
+\ir upgrades/2018-11-18_gift_aid.sql
+\ir upgrades/2018-11-24_tax_factor.sql
+
+\ir test_data/00-currencies.sql
+\ir test_data/01-countries.sql
+\ir test_data/02-charity_categories.sql
+\ir test_data/03-charities.sql
+\ir test_data/04-charities_in_countries.sql
